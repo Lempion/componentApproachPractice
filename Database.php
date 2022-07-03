84ad9643c9a00913e57838fc986373472a1b2498
Vitaly <vit.fedor2012@mail.ru>
2022-07-03 13:53:05 +0300

    - Создан класс(компонент) Config.php

--- Database.php
+++ Database.php
@@ -9,7 +9,7 @@ class Database
     private function __construct()
     {
         try {
-            $this->pdo = new PDO('mysql:host=localhost;dbname=comppractice', 'root', '');
+            $this->pdo = new PDO("mysql:host=" . Config::get('mysql.host') .";dbname=" . Config::get('mysql.database'), Config::get('mysql.username'), Config::get('mysql.password'));
         } catch (PDOException $exception) {
             die($exception->getMessage());
         }
--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 require_once "Database.php";
+require_once "Config.php";
 
-//$users = Database::getInstance()->query("SELECT * FROM users");
+$GLOBALS['config'] = [
+    'mysql' => [
+        'host' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'comppractice'
+    ]
+];
+
+$users = Database::getInstance()->query("SELECT * FROM users");
 //$users = Database::getInstance()->query("SELECT * FROM users WHERE name IN (?)",['John Snow']);
 //$users = Database::getInstance()->get('users', ['name', '=', 'Iliza Ver']);
 //Database::getInstance()->delete('users', ['password', '=', '123321']);
@@ -9,11 +19,15 @@ require_once "Database.php";
 //$id = 5;
 //$users = Database::getInstance()->update('users', $id, ['name' => 'Glaca Molly','password'=>'6566636'])->query("SELECT * FROM users");
 
-//if ($users->error()) {
-//    echo 'Ошибка выполненяи операции';
-//} else {
-//    echo '<pre>';
-//    var_dump($users->results());
-//    echo '</pre>';
-//}
+
+
+//echo "mysql:host=" . Config::get('mysql.host') . ";dbname=" . Config::get('mysql.database'); die();
+
+if ($users->error()) {
+    echo 'Ошибка выполненяи операции';
+} else {
+    echo '<pre>';
+    var_dump($users->results());
+    echo '</pre>';
+}
 

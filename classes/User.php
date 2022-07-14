e7eabe575448c53e522aaeeca0f50805c24a2da4
Vitaly <vit.fedor2012@mail.ru>
2022-07-14 16:22:33 +0300

    - Обновил класс User.php * Обновил функции login, logout, __construct * Добавил функцию exists
    - В init.php сделал проверку на авторизацию пользователя
    
    - Добавил класс Cookie.php

--- classes/User.php
+++ classes/User.php
@@ -2,12 +2,13 @@
 
 class User
 {
-    private $db, $data, $session_name, $isLoggedIn;
+    private $db, $data, $session_name, $cookie_name, $isLoggedIn;
 
     public function __construct($user = null)
     {
         $this->db = Database::getInstance();
         $this->session_name = Config::get('session.user_session');
+        $this->cookie_name = Config::get('cookie.cookie_name');
 
         if (!$user) {
             if (Session::exist($this->session_name)) {
@@ -29,23 +30,55 @@ class User
         return $this->db->insert('users', $fields);
     }
 
-    public function login($email = null, $password = null)
+    public function login($email = null, $password = null, $remember = false)
     {
-        if ($email) {
+        if (!$email && !$password && $this->exists()) {
+            Session::put($this->session_name, $this->data()->id);
+        } else {
+
             if ($this->find($email)) {
                 if (password_verify($password, $this->data()->password)) {
                     Session::put($this->session_name, $this->data()->id);
+
+                    if ($remember) {
+                        $hash = hash('sha256', uniqid());
+
+                        $hashCheck = $this->db->get('user_sessions', ['user_id', '=', $this->data()->id]);
+
+                        if (!$hashCheck->count()) {
+                            $this->db->insert('user_sessions', [
+                                'user_id' => $this->data()->id,
+                                'hash' => $hash,
+                            ]);
+                        } else {
+                            $hash = $hashCheck->first()->hash;
+                        }
+
+                        Cookie::put($this->cookie_name, $hash, Config::get('cookie.cookie_expiry'));
+
+                    }
+
                     return true;
+
                 }
             }
+
         }
+
 
         return false;
     }
 
     public function logout()
     {
+        $this->db->delete('user_sessions', ['user_id', '=', $this->data()->id]);
         Session::delete($this->session_name);
+        Cookie::delete($this->cookie_name);
+    }
+
+    public function exists()
+    {
+        return (!empty($this->data()) ? true : false);
     }
 
     public function find($value)
--- login.php
+++ login.php
@@ -17,11 +17,14 @@ if (Input::exists()) {
 
         if ($validate->passed()) {
             $user = new User();
-            $answer = $user->login(Input::get('email'),Input::get('password'));
-            if($answer){
+            $remember = (Input::get('remember') === 'on' ? true : false);
+
+            $answer = $user->login(Input::get('email'), Input::get('password'), $remember);
+
+            if ($answer) {
                 Session::flash('success', 'Пользователь успешно авторизован');
                 Redirect::to('/');
-            }else{
+            } else {
                 Session::flash('error', 'Ошибка авторизации пользователя');
             }
 
@@ -39,10 +42,23 @@ if (Input::exists()) {
     <p><?php echo Session::flash('error'); ?></p>
 </div>
 <form action="login.php" method="post">
-    <label for="email">Почта</label>
-    <input type="email" name="email" id="email" value="<?php echo Input::get('email'); ?>">
-    <label for="password">Пароль</label>
-    <input type="text" name="password" id="password">
+    <div>
+        <label for="email">Почта</label>
+        <input type="email" name="email" id="email" value="<?php echo Input::get('email'); ?>">
+    </div>
+    <br>
+    <div>
+        <label for="password">Пароль</label>
+        <input type="text" name="password" id="password">
+    </div>
+    <br>
+    <div>
+        <label for="remember">Запомнить меня</label>
+        <input type="checkbox" name="remember" id="remember">
+    </div>
+
     <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
     <button type="submit">Отправить</button>
+
+
 </form>
